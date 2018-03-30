86ca0cda4f4c1b6e3ccf0e806c28d7221c54ee2a
CowboyLynk <cowboylynk@icloud.com>
2018-03-30 16:32:50 -0400

    Added notifications

--- GraphTV/GetGraphData/main.py
+++ GraphTV/GetGraphData/main.py
@@ -4,7 +4,6 @@ import numpy as np
 def get_data(episodes):
 	counter = 1
 	data = []
-	d_colors = []
 	colors = ["#46B8AF", "#CE5858", "#5869CE", "#BD4EAC"]
 	for episode in episodes:
 		season_num = episode.season.season_num
@@ -12,10 +11,8 @@ def get_data(episodes):
 			entry = [counter, episode.rating, 'Rating: {}\nS{}E{} - {}\n'.format(episode.rating, episode.season.season_num, episode.ep_num, episode.title)]
 			color = colors[season_num%len(colors)]
 			entry.append("point { size: 5; fill-color: " + color + "; }")
-			d_colors.append(colors[season_num%len(colors)])
 			data.append(entry)
 		counter += 1
-	print(d_colors)
-	return data, d_colors
+	return data
 
 
--- GraphTV/imdb_database/views.py
+++ GraphTV/imdb_database/views.py
@@ -9,7 +9,7 @@ import GetGraphData.main as p_data
 def home(request):
 	sid = request.GET.get("show_id")  # show id
 	if not sid:
-		return HttpResponse("nah fam")
+		return render(request, "home.html")
 
 	show = Show.objects.all().filter(show_id=sid)[0]
 	seasons = show.season_set.all()
@@ -19,25 +19,6 @@ def home(request):
 		for episode in season.episode_set.all().order_by("ep_num"):
 			episodes.append(episode)
 
-	da_data = [
-		[0, 67], [1, 88], [2, 77],
-		[3, 93], [4, 85], [5, 91],
-		[6, 71], [7, 78], [8, 93],
-		[9, 80], [10, 82],[0, 75],
-		[5, 80], [3, 90], [1, 72],
-		[5, 75], [6, 68], [7, 98],
-		[3, 82], [9, 94], [2, 79],
-		[2, 95], [2, 86], [3, 67],
-		[4, 60], [2, 80], [6, 92],
-		[2, 81], [8, 79], [9, 83],
-		[3, 75], [1, 80], [3, 71],
-		[3, 89], [4, 92], [5, 85],
-		[6, 92], [7, 78], [6, 95],
-		[3, 81], [0, 64], [4, 85],
-		[2, 83], [3, 96], [4, 77],
-		[5, 89], [4, 89], [7, 84],
-		[4, 92], [9, 98]]
+	da_data = p_data.get_data(episodes)
 
-	da_data, colors = p_data.get_data(episodes)
-
-	return render(request, "home.html", {"show": show, "episodes": episodes, "da_data": da_data, "colors": colors})
+	return render(request, "chart_page.html", {"show": show, "episodes": episodes, "da_data": da_data})
